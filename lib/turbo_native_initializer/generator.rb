43855d5144a5af1906408bfa3d7a2202ce503614
Nixon <lazaronixon@hotmail.com>
2023-08-26 17:55:22 -0300

    ios stack template

--- lib/turbo_native_initializer/generator.rb
+++ lib/turbo_native_initializer/generator.rb
@@ -4,8 +4,7 @@ module TurboNativeInitializer
   class Generator < Thor::Group
     include Thor::Actions
 
-    argument :name, desc: "The project name"
-
+    argument :name
     class_option :platform,   type: :string, enum: %w[ios android], required: true
     class_option :navigation, type: :string, enum: %w[stack tabs],  default: "stack"
 
@@ -16,10 +15,19 @@ module TurboNativeInitializer
     end
 
     def copy_template_files
-      directory "#{options[:platform]}_#{options[:navigation]}", name
-    end
-
-    def rename_template_files
+      if options[:platform] == "ios" && options[:navigation] == "stack"
+        # /TurboNativeProject/TurboNativeProject/...
+        directory "ios_stack/TurboNativeProject/Controllers", "#{name}/#{name}/Controllers"
+        directory "ios_stack/TurboNativeProject/Delegates", "#{name}/#{name}/Delegates"
+        directory "ios_stack/TurboNativeProject/Resources", "#{name}/#{name}/Resources"
+        copy_file "ios_stack/TurboNativeProject/Info.plist", "#{name}/#{name}/Info.plist"
+        copy_file "ios_stack/TurboNativeProject/path-configuration.json", "#{name}/#{name}/path-configuration.json"
+        template  "ios_stack/TurboNativeProject/TurboNativeProject.swift", "#{name}/#{name}/#{name}.swift"
+        # /TurboNativeProject/TurboNativeProject.codeproj
+        directory "ios_stack/TurboNativeProject.xcodeproj", "#{name}/#{name}.xcodeproj"
+      else
+        say "Template not implemented"
+      end
     end
   end
 end
---
+++ lib/turbo_native_initializer/templates/ios_stack/TurboNativeProject/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+import UIKit
+import Turbo
+
+final class ViewController: VisitableViewController, ErrorPresenter {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.backButtonDisplayMode = .minimal
+        
+        view.backgroundColor = .systemBackground
+        
+        if presentingViewController != nil {
+            navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .stop, target: self, action: #selector(dismissModal))
+        }
+    }
+    
+    @objc func dismissModal() {
+        dismiss(animated: true)
+    }
+}
