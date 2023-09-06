f8872348fac099dd7018611754d51bf69d4c88c3
Nixon <lazaronixon@hotmail.com>
2023-09-05 23:38:02 -0300

    more work

--- lib/turbo_native_initializer/generator.rb
+++ lib/turbo_native_initializer/generator.rb
@@ -17,36 +17,44 @@ module TurboNativeInitializer
     end
 
     def copy_template_files
-      if ios?
+      case options.platform
+      when "android"
+        copy_android_files
+      when "ios"
+        copy_ios_files
+      end
+    end
+
+    private
+      def project
+        "#{options[:platform]}_#{options[:navigation]}"
+      end
+
+      def copy_ios_files
         # project/TurboNativeProject
         directory "#{project}/TurboNativeProject/Configuration", "#{name}/#{name}/Configuration"
         directory "#{project}/TurboNativeProject/Controllers", "#{name}/#{name}/Controllers"
         directory "#{project}/TurboNativeProject/Delegates", "#{name}/#{name}/Delegates"
         directory "#{project}/TurboNativeProject/Resources", "#{name}/#{name}/Resources"
         template  "#{project}/TurboNativeProject/TurboNativeProject.swift", "#{name}/#{name}/#{name}.swift"
-
         # project
         directory "#{project}/TurboNativeProject.xcodeproj", "#{name}/#{name}.xcodeproj"
       end
 
-      if android?
+      def copy_android_files
         # project/app
         copy_file "#{project}/app/.gitignore", "#{name}/app/.gitignore"
         copy_file "#{project}/app/proguard-rules.pro", "#{name}/app/proguard-rules.pro"
         template  "#{project}/app/build.gradle.kts", "#{name}/app/build.gradle.kts"
-
         # project/app/src/debug
         directory "#{project}/app/src/debug", "#{name}/app/src/debug"
-
         # project/app/src/main
         directory "#{project}/app/src/main/assets", "#{name}/app/src/main/assets"
         directory "#{project}/app/src/main/java/dev/hotwire/turbo/turbonativeproject", "#{name}/app/src/main/java/#{package_path}"
         directory "#{project}/app/src/main/res", "#{name}/app/src/main/res"
         template  "#{project}/app/src/main/AndroidManifest.xml", "#{name}/app/src/main/AndroidManifest.xml"
-
         # project/gradle
         directory "#{project}/gradle", "#{name}/gradle"
-
         # project
         copy_file "#{project}/build.gradle.kts", "#{name}/build.gradle.kts"
         copy_file "#{project}/gradle.properties", "#{name}/gradle.properties"
@@ -54,20 +62,6 @@ module TurboNativeInitializer
         copy_file "#{project}/gradlew.bat", "#{name}/gradlew.bat"
         copy_file "#{project}/local.properties", "#{name}/local.properties"
         template  "#{project}/settings.gradle.kts", "#{name}/settings.gradle.kts"
-      end
-    end
-
-    private
-      def ios?
-        options[:platform] == "ios"
-      end
-
-      def android?
-        options[:platform] == "android"
-      end
-
-      def project
-        "#{options[:platform]}_#{options[:navigation]}"
       end
 
       def package_path
